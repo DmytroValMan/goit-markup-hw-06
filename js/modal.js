37f14b899e3f418393e285122ae490fb164099f4
DmytroValMan <dmitri.m2011@gmail.com>
2024-11-27 14:45:05 +0200

    first commit

---
+++ js/modal.js
@@ -0,0 +1,29 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+        refs.modal.classList.toggle("is-open");
+  }
+})();
+
+(() => {
+  const refs = {
+        openModalBtn: document.querySelector("[menu-modal-open]"),
+        closeModalBtn: document.querySelector("[menu-modal-close]"),
+        modal: document.querySelector("[menu-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+        refs.modal.classList.toggle("is-open");
+  }
+})();
